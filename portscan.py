bf772966c031ad2e2a027dc1c5c5940bfeada78f
Elliot Morgan <elliotmrgn@gmail.com>
2024-06-22 19:46:15 -0400

    minimal comments for portscan

--- portscan.py
+++ portscan.py
@@ -1,10 +1,12 @@
 import socket
+
 class PortScan():
     def __init__(self):
         self.target_ip = ""
         self.target_ports = set()
     
     def get_target_ip(self):
+        # Gets IP to scan 
         target = input("Enter target IP or domain name to scan: ")
         try:
             target_ip = socket.gethostbyname(target)
@@ -13,8 +15,8 @@ class PortScan():
             print(f"Error resolving hostname '{target}': {e}")
             return
         
-    # need input validation
     def get_target_ports(self):
+        # Gets range of ports to scan
         user_defined_ports = input("Enter target ports to scan (seperate numbers and ranges by comma): ").split(',')
         for port in user_defined_ports:
             if '-' in port:
@@ -24,10 +26,12 @@ class PortScan():
                 self.target_ports.add(int(port.strip()))
     
     def scan(self):
+        # Attempts to connect to each targeted port on target IP, outputs result
         for port in self.target_ports:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(.1)  # Set timeout for socket connection
             result = sock.connect_ex((self.target_ip, port))
+            # 0 = no error connecting (port is open)
             if result == 0:
                 print(f"Port {port} is open")
                 sock.close()
